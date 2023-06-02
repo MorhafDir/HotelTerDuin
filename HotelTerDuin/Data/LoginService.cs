2f838fee73ded285e740a98f60f930452a9da46b
hamoodhabib <2066641@talnet.nl>
2023-06-02 09:56:49 +0200

    update

--- HotelTerDuin/Data/LoginService.cs
+++ HotelTerDuin/Data/LoginService.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Security.Cryptography;
 using MySql.Data.MySqlClient;
 
@@ -5,34 +6,54 @@ namespace HotelTerDuin.Data
 {
     public class LoginService
     {
-        public bool Login(string voornaam, string password)
+        public bool Login(string voornaam, string password, out bool isAdmin)
         {
             using (var connection = DatabaseConnection.GetConnection())
             {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
                 using (var command = connection.CreateCommand())
                 {
                     command.CommandText = "SELECT COUNT(*) FROM klant WHERE voornaam = @user AND password = @password";
                     command.Parameters.AddWithValue("@user", voornaam);
                     command.Parameters.AddWithValue("@password", password);
                     var result = command.ExecuteScalar();
-                    return (long)result == 1;
+                    var count = Convert.ToInt64(result);
+
+                    if (count == 1)
+                    {
+                        command.CommandText = "SELECT admin FROM klant WHERE voornaam = @user";
+                        var isAdminResult = command.ExecuteScalar();
+                        isAdmin = Convert.ToBoolean(isAdminResult);
+                        return true;
+                    }
                 }
             }
+
+            isAdmin = false;
+            return false;
         }
 
         public void Register(string voornaam, string password)
         {
             using (var connection = DatabaseConnection.GetConnection())
             {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
                 using (var command = connection.CreateCommand())
                 {
-                    command.CommandText = "insert into klant (voornaam, password) values (@user, @password)";
+                    command.CommandText = "INSERT INTO klant (voornaam, password) VALUES (@user, @password)";
                     command.Parameters.AddWithValue("@user", voornaam);
                     command.Parameters.AddWithValue("@password", password);
-                    command.ExecuteScalar();
+                    command.ExecuteNonQuery();
                 }
             }
         }
-
     }
-    }
+}
